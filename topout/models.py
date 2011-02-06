ba0b76cd94d340a2546ae3738335171897360553
Ian Whalen <ian.whalen@gmail.com>
2011-02-06 00:11:06 -0500

    Cleaned up CHOICES variables into separate file.

--- topout/models.py
+++ topout/models.py
@@ -1,30 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
-
-COLOR_CHOICES=(('#FFFFFF','White'),('#800080','Purple'),('#FF4500','Orange'),
-               ('#0000FF','Blue'), ('#FF1493','Pink'), ('#000000','Black'),
-               ('#FFFF00','Yellow'),('#7CFC00','Neon Green'),('#FF0000','Red'),
-               ('#C0C0C0','Silver'), ('#D1FF03','Neon Yellow'),
-               ('#215E21','Green'))
-
-DIFFICULTY_CHOICES = (('V0-', 'V0-'), ('V0', 'V0'), ('V0+', 'V0+'),
-                      ('V1-', 'V1-'), ('V1', 'V1'), ('V1+', 'V1+'),
-                      ('V2-', 'V2-'), ('V2', 'V2'), ('V2+', 'V2+'),
-                      ('V3-', 'V3-'), ('V3', 'V3'), ('V3+', 'V3+'),
-                      ('V4-', 'V4-'), ('V4', 'V4'), ('V4+', 'V4+'),
-                      ('V5-', 'V5-'), ('V5', 'V5'), ('V5+', 'V5+'),
-                      ('V6-', 'V6-'), ('V6', 'V6'), ('V6+', 'V6+'),
-                      ('V7-', 'V7-'), ('V7', 'V7'), ('V7+', 'V7+'),
-                      ('V8-', 'V8-'), ('V8', 'V8'), ('V8+', 'V8+'),
-                      ('V9-', 'V9-'), ('V9', 'V9'), ('V9+', 'V9+'),
-                      ('V10-', 'V10-'), ('V10', 'V10'), ('V10+', 'V10+'),
-                      ('V11-', 'V11-'), ('V11', 'V11'), ('V11+', 'V11+'),
-                      ('V12-', 'V12-'), ('V12', 'V12'), ('V12+', 'V12+'),
-                      ('V13-', 'V13-'), ('V13', 'V13'), ('V13+', 'V13+'),
-                      ('V14-', 'V14-'), ('V14', 'V14'), ('V14+', 'V14+'),
-                      ('V15-', 'V15-'), ('V15', 'V15'), ('V15+', 'V15+'),
-                      ('V16-', 'V16-'), ('V16', 'V16'), ('V16+', 'V16+'))
+from models_utils import *
 
 class Gym(models.Model):
     gym_name = models.CharField(unique=True, max_length=50)
